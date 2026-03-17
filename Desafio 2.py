8f9a5090686dcc881b5d1c9cbb4bcf62dea66765
Pazamai674 <pedroandra746@gmail.com>
2026-03-17 09:52:22 -0300

    aula 2 - variaveis

--- Desafio 2.py
+++ Desafio 2.py
@@ -1,3 +1,5 @@
 dia =(int(input("digite o dia do seu nascimento")))
 mes =(int(input("digite o mes do seu nascimento(de 1 a 12)")))
-ano =(int(input(("digite o ano do seu nascimento"))))
+ano =(int(input("digite o ano do seu nascimento")))
+
+print(dia, mes, ano)
